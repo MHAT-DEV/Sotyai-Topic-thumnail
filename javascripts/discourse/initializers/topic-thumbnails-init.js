919f258e2b8c58e800f12a26b26199e05b1d9442
MHAT DEV <kokoloman001@gmail.com>
2024-07-22 17:46:51 +0700

    Add files via upload

---
+++ javascripts/discourse/initializers/topic-thumbnails-init.js
@@ -0,0 +1,278 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { inject as service } from "@ember/service";
+import { readOnly } from "@ember/object/computed";
+import discourseComputed, { observes } from "discourse-common/utils/decorators";
+import { once } from "@ember/runloop";
+import {
+  getResolverOption,
+  setResolverOption,
+} from "discourse-common/resolver";
+import { htmlSafe } from "@ember/template";
+
+export default {
+  name: "topic-thumbnails-init",
+  initialize() {
+    this.applyViewClassWorkaround();
+    withPluginApi("0.8.7", (api) => this.initWithApi(api));
+  },
+
+  applyViewClassWorkaround() {
+    // Workaround for https://github.com/discourse/discourse/pull/12685
+    // Can be removed once that has been merged, and sites have had time to update
+    const viewClassKey = Object.keys(requirejs.entries).find((k) =>
+      k.endsWith("raw-views/topic-list-thumbnail")
+    );
+    requirejs.entries["discourse/raw-views/topic-list-thumbnail"] =
+      requirejs.entries[viewClassKey];
+  },
+
+  initWithApi(api) {
+    api.modifyClass("component:topic-list", {
+      pluginId: "topic-thumbnails",
+      topicThumbnailsService: service("topic-thumbnails"),
+      classNameBindings: [
+        "isMinimalGrid:topic-thumbnails-minimal",
+        "isThumbnailGrid:topic-thumbnails-grid",
+        "isThumbnailList:topic-thumbnails-list",
+        "isMasonryList:topic-thumbnails-masonry",
+        "isBlogStyleGrid:topic-thumbnails-blog-style-grid",
+      ],
+      isMinimalGrid: readOnly("topicThumbnailsService.displayMinimalGrid"),
+      isThumbnailGrid: readOnly("topicThumbnailsService.displayGrid"),
+      isThumbnailList: readOnly("topicThumbnailsService.displayList"),
+      isMasonryList: readOnly("topicThumbnailsService.displayMasonry"),
+      isBlogStyleGrid: readOnly("topicThumbnailsService.displayBlogStyle"),
+    });
+
+    const siteSettings = api.container.lookup("site-settings:main");
+
+    if (settings.docs_thumbnail_mode !== "none" && siteSettings.docs_enabled) {
+      api.modifyClass("component:docs-topic-list", {
+        pluginId: "topic-thumbnails",
+        topicThumbnailsService: service("topic-thumbnails"),
+        classNameBindings: [
+          "isMinimalGrid:topic-thumbnails-minimal",
+          "isThumbnailGrid:topic-thumbnails-grid",
+          "isThumbnailList:topic-thumbnails-list",
+          "isMasonryList:topic-thumbnails-masonry",
+          "isBlogStyleGrid:topic-thumbnails-blog-style-grid",
+        ],
+        isMinimalGrid: readOnly("topicThumbnailsService.displayMinimalGrid"),
+        isThumbnailGrid: readOnly("topicThumbnailsService.displayGrid"),
+        isThumbnailList: readOnly("topicThumbnailsService.displayList"),
+        isMasonryList: readOnly("topicThumbnailsService.displayMasonry"),
+        isBlogStyleGrid: readOnly("topicThumbnailsService.displayBlogStyle"),
+      });
+    }
+
+    api.modifyClass("component:topic-list-item", {
+      pluginId: "topic-thumbnails",
+      topicThumbnailsService: service("topic-thumbnails"),
+
+      // Hack to disable the mobile topic-list-item template
+      // Our grid styling is responsive, and uses the desktop HTML structure
+      @observes("topic.pinned")
+      renderTopicListItem() {
+        const wasMobileView = getResolverOption("mobileView");
+        if (
+          wasMobileView &&
+          (this.topicThumbnailsService.displayGrid ||
+            this.topicThumbnailsService.displayMasonry ||
+            this.topicThumbnailsService.displayMinimalGrid ||
+            this.topicThumbnailsService.displayBlogStyle)
+        ) {
+          setResolverOption("mobileView", false);
+        }
+
+        this._super();
+
+        if (wasMobileView) {
+          setResolverOption("mobileView", true);
+        }
+      },
+    });
+
+    //////////////////////////
+    /////////// Masonry Layout
+    //////////////////////////
+    api.modifyClass("component:topic-list", {
+      pluginId: "topic-thumbnails-masonry",
+      topicThumbnailsService: service("topic-thumbnails"),
+
+      masonryTargetColumnWidth: 250,
+      masonryGridSpacingPixels: 10,
+      masonryTitleSpacePixels: 76,
+      masonryDefaultAspect: 1.3,
+      masonryMinAspect: 0.7,
+
+      @discourseComputed("masonryContainerWidth")
+      masonryNumColumns(width) {
+        return Math.floor(width / this.masonryTargetColumnWidth);
+      },
+
+      @discourseComputed(
+        "masonryNumColumns",
+        "masonryContainerWidth",
+        "masonryGridSpacingPixels"
+      )
+      masonryColumnWidth(numColumns, containerWidth, gridSpacing) {
+        return (containerWidth - (numColumns - 1) * gridSpacing) / numColumns;
+      },
+
+      didInsertElement() {
+        this._super();
+        this.updateElementHeight();
+
+        if (window.ResizeObserver) {
+          const observer = new ResizeObserver(() => this.updateElementHeight());
+          observer.observe(this.element);
+          this.set("resizeObserver", observer);
+        }
+      },
+
+      willDestroyElement() {
+        this._super();
+        if (this.resizeObserver) {
+          this.resizeObserver.unobserve(this.element);
+        }
+      },
+
+      updateElementHeight() {
+        this.set(
+          "masonryContainerWidth",
+          this.element.getBoundingClientRect().width
+        );
+      },
+
+      @observes("topics.[]", "masonryContainerWidth")
+      masonryTopicsChanged() {
+        if (!this.topicThumbnailsService.displayMasonry) {
+          return;
+        }
+        if (!this.masonryContainerWidth) {
+          return;
+        }
+        once(this, this.calculateMasonryLayout);
+      },
+
+      calculateMasonryLayout() {
+        const numColumns = this.masonryNumColumns;
+        const gridSpacingPixels = this.masonryGridSpacingPixels;
+
+        const columnHeights = [];
+        for (let n = 0; n < numColumns; n++) {
+          columnHeights[n] = 0;
+        }
+
+        this.filteredTopics.forEach((topic) => {
+
+          console.log(topic);
+          console.log(this);
+
+          // Pick the column with the lowest height
+          const smallestColumn = columnHeights.indexOf(
+            Math.min(...columnHeights)
+          );
+
+          // Get the height of this topic
+          let aspect = this.masonryDefaultAspect;
+          if (topic.thumbnails) {
+            aspect = topic.thumbnails[0].width / topic.thumbnails[0].height;
+          }
+          aspect = Math.max(aspect, this.masonryMinAspect);
+
+    // Change height with title variation
+    let titleHeight = 50;
+
+    if (topic.title.length > 90) {
+      titleHeight = 125;
+    } else if (topic.title.length > 60) {
+      titleHeight = 100;
+    } else if (topic.title.length > 30) {
+      titleHeight = 75;
+    }
+
+    // Change height with tag variation
+    let tagHeight = 0;
+
+    let tagCharacters = topic.tags.join();
+
+    if (tagCharacters <= 30) {
+      tagHeight = 22;
+    }
+    else if (tagCharacters > 60) {
+      tagHeight = 44;
+    }
+    else if (tagCharacters > 90) {
+      tagHeight = 66;
+    }
+
+    let bottomHeight = 65;
+
+    let extraHeight = titleHeight + tagHeight + bottomHeight;
+
+    const thisHeight =
+      this.masonryColumnWidth / aspect + extraHeight;
+
+          topic.set("masonryData", {
+            columnIndex: smallestColumn,
+            height: thisHeight,
+            heightAbove: columnHeights[smallestColumn],
+          });
+
+          columnHeights[smallestColumn] += thisHeight + gridSpacingPixels;
+        });
+
+        this.set("masonryTallestColumn", Math.max(...columnHeights));
+      },
+
+      attributeBindings: ["masonryStyle:style"],
+
+      @discourseComputed(
+        "topicThumbnailsService.displayMasonry",
+        "masonryNumColumns",
+        "masonryGridSpacingPixels",
+        "masonryTallestColumn",
+        "masonryColumnWidth"
+      )
+      masonryStyle(
+        useMasonry,
+        numColumns,
+        gridSpacingPixels,
+        tallestColumn,
+        columnWidth
+      ) {
+        if (!useMasonry) {
+          return;
+        }
+
+        return htmlSafe(
+          `--masonry-num-columns: ${Math.round(numColumns)}; ` +
+            `--masonry-grid-spacing: ${gridSpacingPixels}px; ` +
+            `--masonry-tallest-column: ${Math.round(tallestColumn)}px; ` +
+            `--masonry-column-width: ${Math.round(columnWidth)}px; `
+        );
+      },
+    });
+
+    api.modifyClass("component:topic-list-item", {
+      pluginId: "topic-thumbnails-masonry",
+      attributeBindings: ["masonryStyle:style"],
+
+      @discourseComputed("topic.masonryData")
+      masonryStyle(masonryData) {
+        if (!masonryData) {
+          return;
+        }
+
+        return htmlSafe(
+          `--masonry-height: ${Math.round(masonryData.height)}px; ` +
+            `--masonry-height-above: ${Math.round(
+              masonryData.heightAbove
+            )}px; ` +
+            `--masonry-column-index: ${masonryData.columnIndex};`
+        );
+      },
+    });
+  },
+};
---
+++ javascripts/discourse/services/topic-thumbnails.js
@@ -0,0 +1,169 @@
+import Service, { inject as service } from "@ember/service";
+import discourseComputed from "discourse-common/utils/decorators";
+import Site from "discourse/models/site";
+
+const minimalGridCategories = settings.minimal_grid_categories
+  .split("|")
+  .map((id) => parseInt(id, 10));
+
+const listCategories = settings.list_categories
+  .split("|")
+  .map((id) => parseInt(id, 10));
+
+const gridCategories = settings.grid_categories
+  .split("|")
+  .map((id) => parseInt(id, 10));
+
+const masonryCategories = settings.masonry_categories
+  .split("|")
+  .map((id) => parseInt(id, 10));
+
+const blogStyleCategories = settings.blog_style_categories
+  .split("|")
+  .map((id) => parseInt(id, 10));
+
+const minimalGridTags = settings.minimal_grid_tags.split("|");
+const listTags = settings.list_tags.split("|");
+const gridTags = settings.grid_tags.split("|");
+const masonryTags = settings.masonry_tags.split("|");
+const blogStyleTags = settings.blog_style_tags.split("|");
+
+export default Service.extend({
+  router: service("router"),
+
+  @discourseComputed("router.currentRouteName")
+  isTopicListRoute(currentRouteName) {
+    return (
+      currentRouteName.match(/^discovery\./) ||
+      currentRouteName.match(/^tags?\.show/)
+    );
+  },
+
+  @discourseComputed("router.currentRouteName")
+  isTopicRoute(currentRouteName) {
+    return currentRouteName.match(/^topic\./);
+  },
+
+  @discourseComputed("router.currentRouteName")
+  isDocsRoute(currentRouteName) {
+    return currentRouteName.match(/^docs\./);
+  },
+
+  @discourseComputed(
+    "router.currentRouteName",
+    "router.currentRoute.attributes.category.id"
+  )
+  viewingCategoryId(currentRouteName, categoryId) {
+    if (!currentRouteName.match(/^discovery\./)) {
+      return;
+    }
+    return categoryId;
+  },
+
+  @discourseComputed(
+    "router.currentRouteName",
+    "router.currentRoute.attributes.id", // For discourse instances earlier than https://github.com/discourse/discourse/commit/f7b5ff39cf
+    "router.currentRoute.attributes.tag.id"
+  )
+  viewingTagId(currentRouteName, legacyTagId, tagId) {
+    if (!currentRouteName.match(/^tags?\.show/)) {
+      return;
+    }
+    return tagId || legacyTagId;
+  },
+
+  @discourseComputed(
+    "viewingCategoryId",
+    "viewingTagId",
+    "router.currentRoute.metadata.customThumbnailMode",
+    "isTopicListRoute",
+    "isTopicRoute",
+    "isDocsRoute"
+  )
+  displayMode(
+    viewingCategoryId,
+    viewingTagId,
+    customThumbnailMode,
+    isTopicListRoute,
+    isTopicRoute,
+    isDocsRoute
+  ) {
+    if (customThumbnailMode) {
+      return customThumbnailMode;
+    }
+    if (minimalGridCategories.includes(viewingCategoryId)) {
+      return "minimal-grid";
+    } else if (blogStyleCategories.includes(viewingCategoryId)) {
+      return "blog-style";
+    } else if (masonryCategories.includes(viewingCategoryId)) {
+      return "masonry";
+    } else if (gridCategories.includes(viewingCategoryId)) {
+      return "grid";
+    } else if (listCategories.includes(viewingCategoryId)) {
+      return "list";
+    } else if (masonryTags.includes(viewingTagId)) {
+      return "masonry";
+    } else if (minimalGridTags.includes(viewingTagId)) {
+      return "minimal-grid";
+    } else if (blogStyleTags.includes(viewingTagId)) {
+      return "blog-style";
+    } else if (gridTags.includes(viewingTagId)) {
+      return "grid";
+    } else if (listTags.includes(viewingTagId)) {
+      return "list";
+    } else if (isTopicRoute && settings.suggested_topics_mode) {
+      return settings.suggested_topics_mode;
+    } else if (isTopicListRoute || settings.enable_outside_topic_lists) {
+      return settings.default_thumbnail_mode;
+    } else if (isDocsRoute) {
+      return settings.docs_thumbnail_mode;
+    } else {
+      return "none";
+    }
+  },
+
+  @discourseComputed("displayMode")
+  enabledForRoute(displayMode) {
+    return displayMode !== "none";
+  },
+
+  @discourseComputed()
+  enabledForDevice() {
+    return Site.current().mobileView ? settings.mobile_thumbnails : true;
+  },
+
+  @discourseComputed("enabledForRoute", "enabledForDevice")
+  shouldDisplay(enabledForRoute, enabledForDevice) {
+    return enabledForRoute && enabledForDevice;
+  },
+
+  @discourseComputed("shouldDisplay", "displayMode")
+  displayMinimalGrid(shouldDisplay, displayMode) {
+    return shouldDisplay && displayMode === "minimal-grid";
+  },
+
+  @discourseComputed("shouldDisplay", "displayMode")
+  displayList(shouldDisplay, displayMode) {
+    return shouldDisplay && displayMode === "list";
+  },
+
+  @discourseComputed("shouldDisplay", "displayMode")
+  displayGrid(shouldDisplay, displayMode) {
+    return shouldDisplay && displayMode === "grid";
+  },
+
+  @discourseComputed("shouldDisplay", "displayMode")
+  displayMasonry(shouldDisplay, displayMode) {
+    return shouldDisplay && displayMode === "masonry";
+  },
+
+  @discourseComputed("shouldDisplay", "displayMode")
+  displayBlogStyle(shouldDisplay, displayMode) {
+    return shouldDisplay && displayMode === "blog-style";
+  },
+
+  @discourseComputed("displayMinimalGrid", "displayBlogStyle")
+  showLikes(isMinimalGrid) {
+    return isMinimalGrid;
+  },
+});
